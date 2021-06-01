117a8ecd93df79fbb846983dc4734a39c4f318b8
B. Klara Simon <borbala.klara@gmail.com>
2021-06-01 20:23:54 +0200

    added functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.querySelector("#task-form");
 // ul//
 const taskInput = document.querySelector("#task");
 // ul//
-const taskList = document.querySelector(".task-collection");
+const taskList = document.querySelector(".collection");
 const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 
@@ -17,5 +17,27 @@ function addNewTask(e) {
   if (taskInput.value === "") {
     alert("Please add a task");
   }
+
+  //Create li element
+  const li = document.createElement("li");
+  // Add class from materialize
+  li.className = "collection-item";
+  // Create text node and append
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  //Create new link element
+  const link = document.createElement("a");
+  // add class to link
+  link.className = "delete-item secondary-content";
+  // add icon
+  link.innerHTML = '<i class="far fa-trash-alt"></i>';
+  // append link to li
+  li.appendChild(link);
+
+  // append li to ul
+  taskList.appendChild(li);
+
+  // CLear input
+  taskInput.value = "";
   e.preventDefault();
 }
