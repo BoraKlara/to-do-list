f184519504404fc0574307f85fd27f138e6e3fbb
B. Klara Simon <borbala.klara@gmail.com>
2021-06-03 13:51:56 +0200

    added ls function

--- script.js
+++ script.js
@@ -97,7 +97,26 @@ function removeTask(e) {
     if (confirm("Are you sure you want to delete it? ")) {
       e.target.parentElement.parentElement.remove();
     }
+    // remove tasks from the local storage
+    removeListItemsFromLocalStorage(e.target.parentElement.parentElement);
   }
+}
+
+function removeListItemsFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function (task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem("tasks", JSON.stringify("tasks"));
 }
 
 //Clear all tasks
